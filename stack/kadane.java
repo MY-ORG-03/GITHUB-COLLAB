a8ed80649dc95a86783173b66cca80467dbbcfe1
SrvK21_ <vinayaksaurav284@gmail.com>
2026-04-17 11:29:09 +0530

    kadane algorithm was added by saurav

---
+++ stack/kadane.java
@@ -0,0 +1,22 @@
+public class kadane {
+
+    public static int maxSubArray(int[] arr) {
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            // Either extend previous subarray or start new
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+
+            // Update max sum
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("Maximum Subarray Sum: " + maxSubArray(arr));
+    }
+}
